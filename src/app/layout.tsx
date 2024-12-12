b4f5077f5c7fac3f1d1d9f955f7db79e3b250356
christopherkindl <53372002+christopherkindl@users.noreply.github.com>
2024-12-12 16:31:41 +0100

    add: sev comments

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -70,7 +70,8 @@ export default async function RootLayout({
         >
           <SidebarProvider defaultOpen={defaultOpen}>
             <AppSidebar />
-            <div className="flex-1 overflow-hidden">
+            {/* @sev: placing overflow-hidden here did the table trick, but sticky does not work anymore because of that? */}
+            <div className="w-full overflow-hidden">
               <header className="sticky top-0 z-10 flex h-16 shrink-0 items-center gap-2 border-b border-gray-200 bg-white px-4 dark:border-gray-800 dark:bg-gray-950">
                 <SidebarTrigger className="-ml-1" />
                 <div className="mr-2 h-4 w-px bg-gray-200 dark:bg-gray-800" />
