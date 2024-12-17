aae2f072899e147f466dfc66110c92a347792f3a
severinlandolt <sev.landolt@gmail.com>
2024-12-17 14:25:11 +0100

    update preview

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { SidebarProvider, SidebarTrigger } from "@/components/Sidebar"
 import { AppSidebar } from "@/components/ui/navigation/AppSidebar"
 import { Breadcrumbs } from "@/components/ui/navigation/Breadcrumbs"
+import { RiArrowRightLine } from "@remixicon/react"
 import type { Metadata } from "next"
 import { ThemeProvider } from "next-themes"
 import localFont from "next/font/local"
@@ -50,6 +51,23 @@ export const metadata: Metadata = {
   },
 }
 
+function Banner() {
+  return (
+    <div className="fixed bottom-10 left-1/2 z-50 -translate-x-1/2 transition">
+      <div className="flex items-center gap-x-1 rounded-full bg-gray-950 p-1 text-sm shadow-xl shadow-black/20 ring-1 ring-white/10">
+        <a
+          className="group flex items-center gap-0.5 whitespace-nowrap rounded-[20px] bg-gradient-to-b from-white to-gray-200 px-4 py-2 font-semibold text-gray-900 ring-1 ring-inset ring-indigo-400/30 transition"
+          href="https://blocks.tremor.so/templates#template-planner"
+          target="_blank"
+        >
+          Get this template
+          <RiArrowRightLine />
+        </a>
+      </div>
+    </div>
+  )
+}
+
 export default async function RootLayout({
   children,
 }: {
@@ -80,6 +98,7 @@ export default async function RootLayout({
             </div>
           </SidebarProvider>
         </ThemeProvider>
+        <Banner />
       </body>
     </html>
   )
